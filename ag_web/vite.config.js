18783c97a7253688fd8058207f9f357e1b91e4a8
Ridan Hawy <113379842+Hawy08@users.noreply.github.com>
2024-02-24 14:50:21 +0300

    Deployment

--- ag_web/vite.config.js
+++ ag_web/vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/ag_ghala_web/ag_web/",
   assetsInclude: [
     "**/*.png",
     "**/*.jpg",
